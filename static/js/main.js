2e24fa383fc42f5ac4de22004c04ae870b98dbbc
Chong Han <johncch@gmail.com>
2011-07-21 11:07:42 -0400

    Major change, update to 1.5 Upgraded jquery to 1.6.2 Removed back button and replaced with arrow Rewrote to support multi level content stacks Cleaned up code and added basic comments Minor cleanup of HTML/CSS

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,40 @@
+// System Constants
+VERSION = 1.1;
 BLOG_URL = "core/";
 
+// Constants
 MONTHS = ["January", "February", "March", "April", "May", "June",
 	   "July", "August", "September", "October", "November", "December"];
 
 FIRST_PAGE_LEFT = 0; //px
+LEVEL_PAGE_OFFSET = 170;
 SECOND_PAGE_LEFT = 170; //px
 ANIMATION_OFFSET = 50; //px
-
 ANIMATION_DURATION = 1000; //ms
+FADE_OUT_TIME = 800;
+CONTENT_BOTTOM_PAD = 150;
+NO_REQUEST_TXID = -255;
 
-var customFields = {
+var CUSTOM_FIELDS = {
 	"client": "Client", 
 	"project_timeline": "Project Timeline", 
 	"responsible_for": "Responsible For",
 	"other_info": "Other Information"
 };
-var allCustomFields;
+var ALL_CUSTOM_FIELDS;
+var ACTION_MAP = {
+	"post": "get_post",
+	"page": "get_page",
+	"category": "get_category_posts"
+}
+
 var contentWrapperHeight;
 
+var activeContents = [];
+var curActiveContent = 0;
+var curLoadScreenIndex = 0;
+
+// remove console.log errors
 if(!console) {
 	var console = {
 		log: function(){}
@@ -29,17 +46,18 @@ jQuery.fx.interval = 24;
 
 var currentLink = "";
 var curPageRequestId = 0;
-
-var activeContent = null;
-var secondaryContent = null;
-var contentGlass = null;
-var contentFixed = null;
-
 var aDiv;
 
 $(document).ready(function() {
-	console.log("Hi! Welcome to fifthrevision.com.");
+	console.log("Hi! Welcome to fifthrevision.com version " + VERSION);
 	$("#notice").remove();
+
+	ALL_CUSTOM_FIELDS = "";
+	for(key in CUSTOM_FIELDS) {
+		if(CUSTOM_FIELDS.hasOwnProperty(key)) {
+			ALL_CUSTOM_FIELDS += (ALL_CUSTOM_FIELDS === "") ? key : "," + key;
+		}
+	}
 
 	var isiPad = navigator.userAgent.match(/iPad/i) != null;
 	if(isiPad) {
@@ -57,65 +75,52 @@ $(document).ready(function() {
 		navigationHandler(href);
 	});
 
-	$("#list-portfolio a").live("mouseover", function(e) {
-		var curTarget = $(e.currentTarget);
-		var alt = curTarget.attr("alt");
-		if(!alt) return;
+	$(".content-glass").live("click", glassClickHandler);
+	$(".back-button").live("click", backButtonClickHandler);
 
-		var pos = curTarget.position();
-
-		aSpan = $("<span style='display:none;font-size:80%'>");
-		aSpan.text(alt);
-		$("body").append(aSpan);
-
-		var w = aSpan.width() + 10;
-
-		aSpan.remove();
-
-		aDiv = $("<div class='hover-alt'>").css("position", "absolute");
-		aDiv.css("left", pos.left + curTarget.width() + 10).css("top", pos.top);
-		aDiv.css("width", 1);
-		aDiv.append($("<div>").css("width", w).text(alt));
-		aDiv.animate({
-			width: w
-		}, 100);
-		$("body").append(aDiv);
-	});
-
-	$("#list-portfolio a").live("mouseout", function(e) {
-		aDiv.animate({
-			width: 0
-		}, 100, function() {
-			$(this).remove();
-		});
-	});
+	$("#list-portfolio a").live("mouseover", mouseoverHandler);
+	$("#list-portfolio a").live("mouseout", mouseoutHandler);
 
 	$(".content a:has(img)").live("click", function(e) {
 		e.preventDefault();
 		var url = $(e.currentTarget).attr("href");
 		var image = $("<img>").attr("src", url);
-		setContentToSupportPage(image);
+		// setContentToSupportPage(image);
+		curPageRequestId = NO_REQUEST_TXID;
+		setContentToPage(curActiveContent)(image, NO_REQUEST_TXID);
+	});
+	$(".content a").live("click", function(e) {
+		var href = $(e.currentTarget).attr("href");
+		if (href.substr(0,2) != "#!") 
+			return;
+
+		href = href.substr(3);
+		var urls = href.split("/");
+		var action = urls[0];
+
+		curPageRequestId = request(BLOG_URL, toRequestString({
+			"json": ACTION_MAP[action],
+			"slug": urls[1],
+			"&custom_fields": ALL_CUSTOM_FIELDS,
+			"dev": 1 // This is super weird, JSON plugin is fucked up. need to check
+		}), setContentToPage(curActiveContent));
 	});
 
-	secondaryContent = $("#secondary-content");
-	contentGlass = $("#content-glass");
-	contentFixed = $(".content-fixed");
-	contentGlass.click(function() {
-		contentGlass.css("display", "none");
-		fadeOutSupportPage();
-	});
-	$("#back-button").click(function() {
-		contentGlass.css("display", "none");
-		fadeOutSupportPage();
-	});
+	request(BLOG_URL, toRequestString({
+		"json" : "get_category_posts",
+		"category_slug": "portfolio",
+		"include": ["title","slug","custom_fields"],
+		"custom_fields": ["publish_date","alt_text"],
+		"orderby": "meta_value_num",
+		"meta_key": "publish_date"
+	}), setPortfolio);
 
-	allCustomFields = "";
-	for(key in customFields) {
-		allCustomFields += (allCustomFields === "") ? key : "," + key;
-	}
-
-	request(BLOG_URL, "json=get_category_posts&category_slug=portfolio&include=title,slug,custom_fields&custom_fields=publish_date,alt_text&orderby=meta_value_num&meta_key=publish_date", setPortfolio);
-	request(BLOG_URL, "json=get_category_posts&category_slug=writing&include=title,slug&count=5", setWriting);
+	request(BLOG_URL, toRequestString({
+		"json": "get_category_posts",
+		"category_slug": "writing",
+		"include": ["title", "slug"],
+		"count": 5
+	}), setWriting);
 
 	$("#nav-main").animate({
 		opacity: 1	
@@ -140,6 +145,12 @@ function showError() {
 	
 }
 
+/**
+ * Proxy method to request data from the server
+ * url - URL to request relative to the host
+ * queryStr - query string to be appended after url
+ * fn - callback function to call
+ **/
 function request(url, queryStr, fn) {
 	queryStr = queryStr || "";
 	var id = new Date().getTime();
@@ -157,8 +168,10 @@ function request(url, queryStr, fn) {
 	return id;
 }
 
-var curLoadScreenIndex = 0;
-
+/**
+ * Navigation Handler for links clicked on the primary menu
+ * href - link contained within the element
+ **/
 function navigationHandler(href) {
 	if (currentLink == href) return;
 
@@ -184,139 +197,195 @@ function navigationHandler(href) {
 	}
 	curLoadScreenIndex = (++curLoadScreenIndex) % allLoadScreens.length;
 
-	if(activeContent) {
-		if(secondaryContent.css("opacity") > 0) {	
-			secondaryContent.animate({
-				left: SECOND_PAGE_LEFT + ANIMATION_OFFSET,
-				opacity: 0
-			}, 500);
-		}
-
-		activeContent.animate({
-			left: FIRST_PAGE_LEFT + ANIMATION_OFFSET,
+	for(var i = curActiveContent - 1; i >= 0; i--) {
+		var content = activeContents.pop();
+		content.animate({
+			left: FIRST_PAGE_LEFT + i * LEVEL_PAGE_OFFSET + ANIMATION_OFFSET,
 			opacity: 0
 		}, 500, function() {
-			$(this).remove();
+			$(this).remove();	
 		});
-	} else {
-		$("#loading").animate({
-			opacity: 1
-		}, 800);
 	}
+	curActiveContent = 0;
+	$("#loading").animate({
+		opacity: 1
+	}, 800);
 
 	currentLink = href;
 	if(currentLink == "#!/resume") {
 		var content = '<iframe src="http://crocodoc.com/CNDALE2?embedded=true" width="800" height="1070" style=""></iframe>';
-		curPageRequestId = -1;
-		setContentToPage(content, curPageRequestId);
+		curPageRequestId = NO_REQUEST_TXID;
+		setContentToPage(0)(content, NO_REQUEST_TXID);
 		return;
 	}
 
 	href = href.substr(3);
 	var urls = href.split("/");
 	var action = urls[0];
-	var query = "slug=" + urls[1];
-	if(action == "post") {
-		action = "get_post";
-	} else {
-		action = "get_page";
-	}
 
-	curPageRequestId = request(BLOG_URL, "json=" + action + "&" + query + "&custom_fields=" + allCustomFields + "&dev=1", setContentToPage);
+	curPageRequestId = request(BLOG_URL, toRequestString({
+		"json": ACTION_MAP[action],
+		"slug": urls[1],
+		"&custom_fields": ALL_CUSTOM_FIELDS,
+		"dev": 1 // This is super weird, JSON plugin is fucked up. need to check
+	}), setContentToPage(0));
 }
 
-function setContentToPage(data, txId) {
-	if(txId != curPageRequestId)
-		return; // abandon because new pages have been requested
+/**
+ * Mouse Over Handler for hover state links
+ * e - jquery event Object 
+ */
+function mouseoverHandler(e) {
+	var curTarget = $(e.currentTarget);
+	var alt = curTarget.attr("alt");
+	if(!alt) return;
 
-	var content = $("<div class='content'>");
-	if(data.page) {
-		content.append("<h2>" + data.page.title + "</h2>");
-		content.append(data.page.content);
-	} else if (data.post) {
-		content.append("<h2>" + data.post.title + "</h2>");
+	var pos = curTarget.position();
 
-		var metadata = $("<div class='metadata'>");
-		var ul = $("<ul>");
-		for(key in data.post.custom_fields) {
-			if(data.post.custom_fields.hasOwnProperty(key)) {
-				var li = $("<li>").html("<span class='header'>" + customFields[key] + ": </span>" + data.post.custom_fields[key]);
+	aSpan = $("<span style='display:none;font-size:80%'>");
+	aSpan.text(alt);
+	$("body").append(aSpan);
+
+	var w = aSpan.width() + 10;
+
+	aSpan.remove();
+
+	aDiv = $("<div class='hover-alt'>").css("position", "absolute");
+	aDiv.css("left", pos.left + curTarget.width() + 10).css("top", pos.top);
+	aDiv.css("width", 1);
+	aDiv.append($("<div>").css("width", w).text(alt));
+	aDiv.animate({
+		width: w
+	}, 100);
+	$("body").append(aDiv);
+}
+
+/**
+ * Mouse Out Handler for hover state links
+ * e - jquery event Object 
+ */
+function mouseoutHandler(e) {
+	aDiv.animate({
+		width: 0
+	}, 100, function() {
+		$(this).remove();
+	});
+}
+
+function setContentToPage(level) {
+	level = level || 0;
+	return function(data, txId) {
+		if(txId != curPageRequestId)
+			return; // abandon because new pages have been requested
+
+		var content = $("<div class='content'>");
+		if(level > 0) {
+			content.append($("<div class='back-button'>"));
+		}
+
+		if(data.page) {
+			content.append("<h2>" + data.page.title + "</h2>");
+			content.append(data.page.content);
+		} else if (data.post) {
+			content.append("<h2>" + data.post.title + "</h2>");
+
+			var metadata = $("<div class='metadata'>");
+			var ul = $("<ul>");
+			for(key in data.post.custom_fields) {
+				if(data.post.custom_fields.hasOwnProperty(key)) {
+					var li = $("<li>").html("<span class='header'>" + CUSTOM_FIELDS[key] + ": </span>" + data.post.custom_fields[key]);
+					ul.append(li);
+				}
+			}
+			content.append(metadata.append(ul));
+			content.append(data.post.content);
+
+			if(data.post.categories[0].slug == "writing") {
+				var postDate = data.post.date;
+				var year = postDate.substr(0, 4);
+				var month = parseInt(postDate.substr(5,2)) - 1;
+				var date = postDate.substr(8, 2);
+				var strDate = "- " + MONTHS[month] + " " + date + ", " + year;
+				content.append($("<div class='date'>").text(strDate));
+				content.append($("<div class='signature'>"));
+			}
+		} else if (data.posts) { 
+			content.append("<h2>" + data.category.title + "</h2>");
+			var ul = $("<ul class='post-list'>");
+			for (var i = 0; i < data.posts.length; i++) {
+				var a = $("<a>").attr("href", "#!/post/" + data.posts[i].slug).text(data.posts[i].title);
+				var li = $("<li>").append(a);
 				ul.append(li);
 			}
+			content.append(ul);
+		} else {
+			content.append(data);
 		}
-		content.append(metadata.append(ul));
+		
+		content.append($("<div class='clear'>"));
+		var contentWrapper = $("<div class='content-wrapper'>");
+		contentWrapper.addClass("content-wrapper-level-" + level);
+		contentWrapper.append(content);
+		contentWrapper.append("<div class='content-glass'>");
 
-		content.append(data.post.content);
-
-		if(data.post.categories[0].slug == "writing") {
-			var postDate = data.post.date;
-			var year = postDate.substr(0, 4);
-			var month = parseInt(postDate.substr(5,2)) - 1;
-			var date = postDate.substr(8, 2);
-			var strDate = "- " + MONTHS[month] + " " + date + ", " + year;
-			content.append($("<div class='date'>").text(strDate));
-			content.append($("<div class='signature'>"));
+		if(level == 0) {
+			$("#loading").after(contentWrapper);
+		} else {
+			activeContents[level - 1].after(contentWrapper);
 		}
-	} else {
-		content.append(data);
-	}
-	content.append($("<div class='clear'>"));
-	var contentWrapper = $("<div class='content-wrapper'>");
-	contentWrapper.append(content);
-	$("#loading").after(contentWrapper);
-	contentWrapper.css({
-		left: FIRST_PAGE_LEFT + ANIMATION_OFFSET,
-		opacity: 0,
-	});
-	activeContent = contentWrapper;
-	resizeContentWrappers();
-	contentWrapper.animate({
-		left: FIRST_PAGE_LEFT,
-		opacity: 1
-	}, 1000);
-}
 
-function setContentToSupportPage(data) {
-	contentGlass.css("display", "block");
-
-	var content = secondaryContent.find(".content .content-inner");
-	content.empty();
-	content.append(data);
-
-	secondaryContent.css({
-		left: SECOND_PAGE_LEFT + ANIMATION_OFFSET,
-		opacity: 0,
-		display: "block"
-	});
-	resizeContentWrappers();
-	secondaryContent.animate({
-		left: SECOND_PAGE_LEFT,
-		opacity: 1
-	}, 1000);
-	contentGlass.css("height", contentWrapperHeight);
-	// positionSecondaryContent();
-}
-
-function positionSecondaryContent() {
-		var h = contentFixed.height();
-		var t = $(window).scrollTop();
-		var targetH = (h + t > contentWrapperHeight - 150) ? contentWrapperHeight - h - 150 : t;
-		contentFixed.stop().animate({
-			top: targetH
-		}, "slow");
-}
-
-function fadeOutSupportPage() {
-	secondaryContent.animate({
-		left: SECOND_PAGE_LEFT + ANIMATION_OFFSET,
-		opacity: 0
-	}, 1000, function() {
-		secondaryContent.css({
-			display: "none"
+		contentWrapper.css({
+			left: FIRST_PAGE_LEFT + level * LEVEL_PAGE_OFFSET + ANIMATION_OFFSET,
+			opacity: 0,
 		});
-		contentGlass.css("display", "none");
+		// activeContent = contentWrapper;
+		
+		for (var i = 0; i < curActiveContent; i++) {
+			var aContent = activeContents[i];
+			aContent.children(".content-glass").css("display", "block");
+		};
+
+		activeContents.push(contentWrapper);
+		curActiveContent = level + 1;
 		resizeContentWrappers();
-	});
+		contentWrapper.animate({
+			opacity: 1,
+			left: FIRST_PAGE_LEFT + level * LEVEL_PAGE_OFFSET
+		}, 1000);
+	}
+}
+
+function glassClickHandler(e) {
+	var self = $(e.currentTarget);
+	self.css("display", "none");
+	var container = self.parent(".content-wrapper");
+	var strClass = container.attr("class");
+	var longestClass = strClass.split(" ").sort().pop();
+	var numLevel = parseInt(longestClass.charAt(longestClass.length - 1), 10);
+	fadeOutToLevel(numLevel);
+}
+
+function backButtonClickHandler(e) {
+	var self = $(e.currentTarget);
+	var container = self.parent().parent(".content-wrapper");
+	var strClass = container.attr("class");
+	var longestClass = strClass.split(" ").sort().pop();
+	var numLevel = parseInt(longestClass.charAt(longestClass.length - 1), 10);
+	fadeOutToLevel(numLevel - 1);
+}
+
+function fadeOutToLevel(level) {
+	curActiveContent = level + 1;
+	for(var i = activeContents.length; i > curActiveContent; i--) {
+		var content = activeContents.pop();
+		content.animate({
+			left: FIRST_PAGE_LEFT + i * LEVEL_PAGE_OFFSET + ANIMATION_OFFSET,
+			opacity: 0
+		}, FADE_OUT_TIME, function() {
+			$(this).remove();	
+		});
+	}
+	resizeContentWrappers();
 }
 
 function resizeContentWrappers() {
@@ -327,16 +396,12 @@ function resizeContentWrappers() {
 	var height = 0;
 
 	var maxContentDivH = 0;
-	if(activeContent) {
-		var activeContentDiv = activeContent.children(".content").first();
-		maxContentDivH = activeContentDiv.height();
+	for(var i = 0; i < activeContents.length; i++) {
+		var activeContentDiv = activeContents[i].children(".content").first();
+		var h = activeContentDiv.height();
+		maxContentDivH = (maxContentDivH < h) ? h : maxContentDivH; 
 	}
-
-	if (secondaryContent.is(":visible")) {
-		var secContentDiv = secondaryContent.children(".content").first();
-		if(secContentDiv.height() > maxContentDivH) maxContentDivH = secContentDiv.height();
-	}
-	maxContentDivH += 150;
+	maxContentDivH += CONTENT_BOTTOM_PAD;
 
 	contentWrapperHeight = (maxContentDivH > $(window).height()) ? maxContentDivH : $(window).height();
 
@@ -345,12 +410,11 @@ function resizeContentWrappers() {
 	});
 
 	$(".content").each(function() {
-		$(this).css("height", contentWrapperHeight - 150);
+		$(this).css("height", contentWrapperHeight - CONTENT_BOTTOM_PAD);
 	});
 }
 
 // Retrieve list of documents and fade in
-
 var writingSet = false;
 
 function setWriting(data) {
@@ -360,6 +424,9 @@ function setWriting(data) {
 		var li = $("<li>").append(a);
 		writingList.append(li);
 	}
+	var a = $("<a>").attr("href", "#!/category/writing").html("[list all]");
+	var li = $("<li>").append(a);
+	writingList.append(li);
 	writingSet = true;
 	fadeIn();
 }
@@ -393,7 +460,12 @@ function windowResizeHandler() {
 	resizeContentWrappers();
 }
 
-// utils
+// UTILITIES
+
+/**
+ * Preload images
+ * imgs - array of image paths in string
+ **/
 function preloadImage(imgs) {
 	if(imgs.constructor == Array) {
 		for (var i = 0; i < imgs.length; i++) {
@@ -401,4 +473,28 @@ function preloadImage(imgs) {
 			img.src = imgs[i];
 		}
 	}
+}
+
+/**
+ * Convert JSON object into Request parameters. the main use of this method
+ * is to provide readability on AJAX params
+ * obj - js object
+ *
+ * @return
+ * string - converted request string
+ */
+function toRequestString(obj) {
+	var str = "";
+	for(var key in obj) {
+		// check for none null and prototype chain
+		if(obj[key] && obj.hasOwnProperty(key)){ 
+			str += "&" + key + "=";
+			if(obj[key] instanceof Array) {
+				str += obj[key].join(",");
+			} else {
+				str += obj[key];
+			}
+		}
+	}
+	return str.substr(1);
 }
